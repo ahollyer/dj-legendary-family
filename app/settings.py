a22e5f439a97bd8428ea291cca2ba6189c3bbea0
Aspen Hollyer <aspen.dollins@gmail.com>
2017-06-11 09:14:58 -0500

    Set up for deployment

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'dev')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '45.32.162.25',
+    'localhost',
+    'legendary.family',
+    'www.legendary.family',
+]
 
 
 # Application definition
@@ -44,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,6 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static-compiled")
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'app/media')
 
---
+++ fabfile.py
@@ -0,0 +1,29 @@
+from fabric.api import run, env, sudo, cd, prefix
+
+env.hosts = ['45.32.162.25']
+env.user = 'aspen'
+
+DIR = '/home/aspen/dj-legendary-family'
+VENV = 'source /home/aspen/.virtualenvs/legendary/bin/activate && source SECRET.ENV'
+
+def start ():
+  with cd(DIR):
+    with prefix(VENV):
+      run('pm2 start uwsgi -- --ini uwsgi.ini > start.log')
+
+def stop ():
+  run('pm2 stop all > stop.log')
+
+def deploy ():
+  with cd(DIR):
+    run('git pull')
+
+    with prefix(VENV):
+      run('pip install -r requirements.txt  > install.log')
+      run('python manage.py migrate')
+
+    run('pm2 restart all > restart.log')
+
+# Test if correctly installed
+def hello ():
+  print("Hello")
