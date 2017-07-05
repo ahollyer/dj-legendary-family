834451771932e7b44bfe4068ddade71023a9366b
Aspen Hollyer <aspen.dollins@gmail.com>
2017-07-05 15:11:18 -0500

    Add photos and rsvps to admin site

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from main.models import Post, Comment, Like
+from main.models import (Post, Comment, Like,
+                        Photo, PhotoLike, PhotoTag,
+                        Rsvp)
 
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(Like)
+admin.site.register(Photo)
+admin.site.register(PhotoLike)
+admin.site.register(PhotoTag)
+admin.site.register(Rsvp)
